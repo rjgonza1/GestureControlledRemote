62d2af88a61ee4477268632c0b0786f9fa69935e
rjgonza1 <rjgonza1@uci.edu>
2018-01-24 23:30:32 -0800

    Removed extra directories

--- GestureControlledRemote/App.xaml.cs
+++ GestureControlledRemote/App.xaml.cs
@@ -1,24 +1,13 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="App.xaml.cs" company="Rhemyst and Rymix">
-//     Open Source. Do with this as you will. Include this statement or 
-//     don't - whatever you like.
-//
-//     No warranty or support given. No guarantees this will work or meet
-//     your needs. Some elements of this project have been tailored to
-//     the authors' needs and therefore don't necessarily follow best
-//     practice. Subsequent releases of this project will (probably) not
-//     be compatible with different versions, so whatever you do, don't
-//     overwrite your implementation with any new releases of this
-//     project!
-//
-//     Enjoy working with Kinect!
-// </copyright>
-//-----------------------------------------------------------------------
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
-namespace DWTGestureRecognition
+namespace GestureControlledRemote
 {
-    using System.Windows;
-
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
